ef86d7db63780f3f5de504a3dc4f4896cfff757d
Damilola Odunbaku <me@damilola.io>
2021-04-27 19:31:10 +0100

    add sum of left leaves

---
+++ Algo/LeetCode/Trees/SumOfLeftLeaves.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algo.LeetCode.Trees
+{
+    /// <summary>
+    /// 404. Sum of Left Leaves
+    /// </summary>
+    class SumOfLeftLeaves
+    {
+        int sum = 0;
+
+        public int SumLeftLeaves(TreeNode root)
+        {
+            if (root == null) return sum;
+
+            Recurse(root);
+
+            return sum;
+        }
+
+        private void Recurse(TreeNode node)
+        {
+            if (node == null) return;
+
+            if (node.left != null && node.left.left == null && node.left.right == null)
+            {
+                sum += node.left.val;
+            }
+
+            Recurse(node.left);
+            Recurse(node.right);
+        }
+    }
+}
