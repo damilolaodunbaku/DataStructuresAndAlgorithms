4100777890b05f34087e632bde15344edd2db107
Damilola Odunbaku <me@damilola.io>
2021-04-27 20:25:52 +0100

    add length of last word

---
+++ Algo/LeetCode/String/LengthOfLastWord.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algo.LeetCode.String
+{
+    class LengthOfLastWord
+    {
+        public int LengthOfLast_Word(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return 0;
+            }
+
+            int lengthOfLastWord = 0;
+            int currentWordCount = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!char.IsWhiteSpace(s[i]))
+                {
+                    currentWordCount++;
+                    lengthOfLastWord = currentWordCount;
+                }
+                else
+                {
+                    currentWordCount = 0;
+                }
+
+            }
+
+            return lengthOfLastWord;
+        }
+    }
+}
